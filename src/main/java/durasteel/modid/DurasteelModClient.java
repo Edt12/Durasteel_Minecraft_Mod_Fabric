55484b6c5cf4c3e49909a1fa5baf16ebff530034
Edt12 <104518243+Edt12@users.noreply.github.com>
2024-02-10 16:36:14 +0000

    Initial commit

---
+++ src/main/java/durasteel/modid/DurasteelModClient.java
@@ -0,0 +1,14 @@
+package durasteel.modid;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DurasteelModClient implements ClientModInitializer {
+
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
